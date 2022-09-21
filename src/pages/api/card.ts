f5714d13fe70ebf652b1e1eb081cbfe987692bc7
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-21 19:40:32 +0300

    feat: finish list & card endpoints

--- src/pages/api/card.ts
+++ src/pages/api/card.ts
@@ -2,27 +2,20 @@ import { prisma } from "@/lib/prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  if (req.method === "GET") {
-    const lists = await prisma.list.findMany({
-      where: { userId: req.body.userId },
-      include: { cards: true }
-    });
-    return res.status(200).json(lists);
-  }
-
   if (req.method === "POST") {
-    const list = await prisma.list.create({
+    const list = await prisma.card.create({
       data: {
         index: 1,
         title: req.body.title,
-        userId: req.body.userId
+        description: req.body.description,
+        listId: req.body.listId
       }
     });
     return res.status(201).json(list);
   }
 
   if (req.method === "DELETE") {
-    await prisma.list.delete({
+    await prisma.card.delete({
       where: { id: req.body.id }
     });
     res.status(200).end();
