cc8102f43c82899531337a41fc91d90ffef23a93
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-21 19:41:15 +0300

    chore: get rid of email provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { prisma } from "@/lib/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import EmailProvider from "next-auth/providers/email";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -18,19 +17,7 @@ export const authOptions: NextAuthOptions = {
     maxAge: 10 * 12 * 30 * 24 * 60 * 60 // 10 years, it's a damn to-do app
   },
   adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
-      from: process.env.EMAIL_FROM
-    })
-  ],
+  providers: [],
   pages: {
     signIn: "/auth/login",
     signOut: "/auth/logout",
