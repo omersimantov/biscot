1afbfcc2add2d2dd8de8502aa2c6d5b11d821059
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-24 23:23:03 +0300

    feat: add 404 & 500 pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Custom404: NextPage = () => {
+  return (
+    <main className="grid items-center text-center min-h-screen">
+      <h1 className="font-bold text-xl">404</h1>
+    </main>
+  );
+};
+
+export default Custom404;
