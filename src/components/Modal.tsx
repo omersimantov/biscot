adf5271091aff0b7d94d573a4daec34e27749db5
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-24 23:23:50 +0300

    fix: fix modal issues

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -27,14 +27,17 @@ export const Modal = ({ toggle, content }: { content: JSX.Element; toggle: Mouse
   return (
     <div
       ref={bgRef}
-      className="h-screen w-screen bg-neutral-900 cursor-pointer block sm:flex items-center justify-center md:bg-neutral-900/90 overflow-auto fixed left-0 top-0 z-50"
+      className="h-screen w-screen bg-neutral-900 cursor-pointer block md:flex items-center max-h-fit justify-center md:bg-neutral-900/90 overflow-auto fixed left-0 top-0 z-10"
       onClick={closeModalOnBGClick}>
       <XMarkIcon
-        className="w-7 cursor-pointer text-neutral-500 hover:text-white fixed top-0 right-0 m-5 md:m-10"
+        className="hidden md:block w-7 cursor-pointer text-neutral-500 hover:text-white md:fixed top-0 right-0 m-10"
         onClick={toggle}
       />
-      <div className="flex items-center justify-center p-5 sm:p-10 border-border md:h-fit h-full md:border bg-bg md:rounded-lg w-full md:max-w-prose cursor-auto overflow-auto">
-        {content}
+      <div className="overscroll-none p-5 sm:p-10 border-border md:h-fit max-h-full h-full md:border bg-bg md:rounded-lg w-full md:max-w-xl cursor-auto overflow-auto z-50">
+        <div className="flex justify-end md:hidden">
+          <XMarkIcon className="w-7 cursor-pointer text-neutral-500 hover:text-white" onClick={toggle} />
+        </div>
+        <div className="py-5 sm:py-10 md:py-0">{content}</div>
       </div>
     </div>
   );
