2c44198fb5e2baf617b3c382f89004f3b0d8207c
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-25 14:36:05 +0300

    style: minor edit

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 const Custom500: NextPage = () => {
   return (
     <main className="grid items-center text-center min-h-screen">
-      <h1 className="font-bold text-xl">500</h1>
+      <h1 className="font-bold text-2xl">500</h1>
     </main>
   );
 };
