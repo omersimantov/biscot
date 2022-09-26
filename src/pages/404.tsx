9f3ddd699272f31b555a514e14177d546f4c2825
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-26 20:05:13 +0300

    refactor: cleanup

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 const Custom404: NextPage = () => {
   return (
     <main className="grid items-center text-center min-h-screen">
-      <h1 className="font-bold text-2xl">404</h1>
+      <h1 className="font-bold text-xl">404</h1>
     </main>
   );
 };
