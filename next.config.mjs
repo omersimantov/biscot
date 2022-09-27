716bd8e6a79e56bf3f6983a3d32a4d3dacd5498d
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-27 14:11:45 +0300

    refacor: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -5,31 +5,11 @@
  * @constraint {{import('next').NextConfig}}
  */
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function defineNextConfig(config) {
-  return config;
-}
-
-export default defineNextConfig({
+export default {
   reactStrictMode: true,
   swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en"
-  },
-  images: {
-    domains: [process.env.BASE_URL || "http://localhost:3000/"],
-    // TODO: Configure
-    remotePatterns: [
-      // {
-      //   protocol: "https",
-      //   hostname: "example.com",
-      //   port: "",
-      //   pathname: "/account123/**"
-      // }
-    ]
-  },
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   async redirects() {
     return [
       {
@@ -49,4 +29,4 @@ export default defineNextConfig({
       }
     ];
   }
-});
+};
