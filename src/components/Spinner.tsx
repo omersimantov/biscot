b9bd7153d0abf3d523da7e23c302ea31f26f5e40
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-28 19:04:32 +0300

    refactor: cleanup

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 export const Spinner = ({ className }: { className?: string }): JSX.Element => {
   return (
     <svg viewBox="0 0 50 50" className={classNames(className, "mx-auto h-2/5 max-h-6 animate-spinSlow")}>
-      <circle className="animate-dash stroke-white" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
+      <circle className="animate-dash stroke-borderLight" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
     </svg>
   );
 };
