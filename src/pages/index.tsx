b3c08073824aaf8634175fb37d0d32530a7e7341
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-09-28 19:12:35 +0300

    fix: fix errors & finish login flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,19 @@ import { GoogleIcon } from "@/components/GoogleIcon";
 import { Header } from "@/components/Header";
 import { List } from "@/components/List";
 import { ListSkeleton } from "@/components/ListSkeleton";
-import { List as TList } from "@/lib/prisma/client";
+import { Spinner } from "@/components/Spinner";
+import type { List as TList } from "@/lib/prisma/client";
 import { CakeIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
+import cuid from "cuid";
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import { getSession, signIn } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
 
 const Home: NextPage<{ uid: string }> = ({ uid }) => {
+  const [lists, setLists] = useState<TList[]>([]);
+  const [signingIn, setSigningIn] = useState<boolean>(false);
   const endRef = useRef<HTMLDivElement>(null);
-
-  // TODO: Get from session
-  const session = "f";
-  const userId = "cl8bw3e1t00159xpdasnwbgll";
 
   useEffect((): void => {
     fetchLists();
@@ -34,7 +34,7 @@ const Home: NextPage<{ uid: string }> = ({ uid }) => {
       index: lists ? lists.length : 0,
       title: "New List",
       cards: [],
-      userId
+      userId: uid
     };
     lists ? setLists([...lists, newList]) : setLists([newList]);
     // Unclear why the setTimeout is necessary, but it is
@@ -61,7 +61,7 @@ const Home: NextPage<{ uid: string }> = ({ uid }) => {
           {lists ? (
             <>
               {lists.map((list) => (
-                <List key={list.id} {...list} />
+                <List key={list.index} {...list} />
               ))}
               <div ref={endRef} className="px-10 min-h-full min-w-fit">
                 <div
@@ -83,17 +83,30 @@ const Home: NextPage<{ uid: string }> = ({ uid }) => {
         </main>
       ) : (
         <main className="grid px-5 items-center py-10 min-h-[calc(100vh-4rem)]">
-          <div className="space-y-6">
-            <CakeIcon className="w-9 mx-auto" strokeWidth={1} />
-            <div className="max-w-xs text-center text-lg mx-auto">
+          <div className="space-y-6 w-80 mx-auto">
+            <CakeIcon className="w-10 mx-auto" strokeWidth={1} />
+            <div className="text-center text-lg">
               Biscot is a minimal alternative to Trello for people who use it for personal stuff.
             </div>
-            <a
-              className="text-center p-5 mx-auto border-border bg-neutral-800 border rounded-lg w-80 max-w-full 
-            font-bold no-underline items-center flex justify-center space-x-3 hover:border-borderLight">
-              <GoogleIcon />
-              <div>Continue with Google</div>
-            </a>
+            <button
+              disabled={signingIn}
+              onClick={(): void => {
+                setSigningIn(true);
+                signIn("google");
+              }}
+              className={classNames(
+                "h-[4.5rem] border-border bg-neutral-800 border rounded-lg w-full font-bold no-underline items-center flex justify-center space-x-3 hover:border-borderLight",
+                signingIn && "cursor-not-allowed hover:border-border"
+              )}>
+              {signingIn ? (
+                <Spinner />
+              ) : (
+                <>
+                  <GoogleIcon />
+                  <div>Continue with Google</div>
+                </>
+              )}
+            </button>
           </div>
         </main>
       )}
