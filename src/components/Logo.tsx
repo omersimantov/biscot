2a21a242b13fb8fcf4dff0d2aea623983a3d77c8
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-02 19:39:17 +0300

    fix: fix

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,5 +2,5 @@ import { RectangleStackIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 
 export const Logo = ({ className }: { className?: string }): JSX.Element => {
-  return <RectangleStackIcon className={classNames("mx-auto", className)} strokeWidth={1} xlinkTitle="Logo" />;
+  return <RectangleStackIcon className={classNames("mx-auto", className)} strokeWidth={1} aria-label="Logo" />;
 };
