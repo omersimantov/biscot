cc91b306d3c2e05602cac0fac73f2ab75719a5a5
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-04 20:12:50 +0300

    chore: move getFormattedDate to utils

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { Modal } from "@/components/Modal";
 import { useClickOutside } from "@/lib/hooks/useClickOutside";
+import { getFormattedDate } from "@/lib/utils/getFormattedDate";
 import { Bars3BottomLeftIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
 import type { Card as TCard } from "@prisma/client";
 import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
@@ -75,21 +76,6 @@ export const Card = (card: TCard): JSX.Element => {
       document.removeEventListener("keydown", keyPress);
     };
   }, [keyPress]);
-
-  const getFormattedDate = (d: Date): string => {
-    const date = new Date(d);
-    const today = new Date();
-    if (date.toLocaleDateString() === today.toLocaleDateString())
-      return "Today @ " + date.toLocaleTimeString("en-UK", { hour: "2-digit", minute: "2-digit" });
-    const yesterday = new Date(Date.now() - 86400000);
-    if (date.toLocaleDateString() === yesterday.toLocaleDateString())
-      return "Yesterday @ " + date.toLocaleTimeString("en-UK", { hour: "2-digit", minute: "2-digit" });
-    return (
-      date.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" }) +
-      " @ " +
-      date.toLocaleTimeString("en-UK", { hour: "2-digit", minute: "2-digit" })
-    );
-  };
 
   const modalContent = (
     <div className="w-full">
