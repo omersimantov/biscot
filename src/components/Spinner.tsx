dd770d98b9bee3afc8f73989fc8e90dbd3d4ebd0
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-17 18:21:12 +0300

    refactor: revert spinner size

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 export const Spinner = ({ className }: { className?: string }): JSX.Element => {
   return (
-    <svg viewBox="0 0 50 50" className={classNames(className, "mx-auto h-2/5 max-h-[1.6rem] animate-spinSlow")}>
+    <svg viewBox="0 0 50 50" className={classNames(className, "mx-auto h-2/5 max-h-6 animate-spinSlow")}>
       <circle
         className="animate-dash stroke-white pointer-events-none bg-transparent"
         cx="25"
