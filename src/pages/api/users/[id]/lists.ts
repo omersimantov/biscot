e27a6dfb99caee132844b5c871fe1d088ca430f1
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-20 16:27:41 +0300

    chore: add patch to users/lists endpoint

--- src/pages/api/users/[id]/lists.ts
+++ src/pages/api/users/[id]/lists.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma/client";
+import { List, prisma } from "@/lib/prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
@@ -10,5 +10,25 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
       select: { lists: { orderBy: [{ index: "asc" }], include: { cards: { orderBy: { index: "asc" } } } } }
     });
     return res.status(200).json(lists);
+  }
+
+  if (req.method === "PATCH") {
+    const { lists } = req.body;
+    try {
+      await prisma.user.update({
+        where: { id: id as string },
+        data: {
+          lists: {
+            updateMany: lists.map((list: List) => ({
+              where: { id: list.id },
+              data: { index: list.index, title: list.title }
+            }))
+          }
+        }
+      });
+      return res.status(200).json(lists);
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
   }
 };
