cd65b257a6db3630d2b9cf3b69f08e9c1a86517a
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-22 11:34:24 +0300

    refactor: get rid of favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en" dir="ltr">
-        <Head>
+        {/* <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -12,7 +12,7 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
-        </Head>
+        </Head> */}
         <body>
           <Main />
           <NextScript />
