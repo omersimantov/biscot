80919b9456fed09f8edabfbac1c37d828cb8cf13
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-22 11:35:07 +0300

    chore: add get to the card/[id] endpoint

--- src/pages/api/cards/[id].ts
+++ src/pages/api/cards/[id].ts
@@ -4,6 +4,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const { id } = req.query;
 
+  if (req.method === "GET") {
+    const card = await prisma.card.findFirstOrThrow({
+      where: { id: id as string }
+    });
+    return res.status(200).json(card);
+  }
+
   if (req.method === "PATCH") {
     try {
       await prisma.card.update({
