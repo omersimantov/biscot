a2e7d18c01b020e095e2ec797f3e6a32075e21cf
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-22 11:36:56 +0300

    fix: add missing Head tag to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en" dir="ltr">
-        {/* <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <Head>
+          {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#222222" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head> */}
+          <meta name="theme-color" content="#ffffff" /> */}
+        </Head>
         <body>
           <Main />
           <NextScript />
