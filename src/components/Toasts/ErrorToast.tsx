1d8367b37255fccf6ca20f935c729c9b3f36a485
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-24 18:49:22 +0300

    style: increase error icon size

--- src/components/Toasts/ErrorToast.tsx
+++ src/components/Toasts/ErrorToast.tsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 export const errorToast = (): void => {
   toast.custom(
     <div className="rounded-lg p-4 border border-red bg-neutral-900">
-      <ExclamationCircleIcon className="w-4 text-red" strokeWidth={2} />
+      <ExclamationCircleIcon className="w-5 text-red" strokeWidth={2} />
     </div>,
     {
       position: "bottom-left",
