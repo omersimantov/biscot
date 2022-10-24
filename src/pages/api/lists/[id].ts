7bf532420dcea0a11ded62bd3e3cbfa428d5dc53
Omer Siman Tov <73315088+omerst@users.noreply.github.com>
2022-10-24 19:40:01 +0300

    fix: fix lists/[id] endpoint

--- src/pages/api/lists/[id].ts
+++ src/pages/api/lists/[id].ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma/client";
+import { Card } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
@@ -13,12 +14,22 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
   }
 
   if (req.method === "PATCH") {
+    const { cards } = req.body;
     try {
-      await prisma.list.update({
+      const list = await prisma.list.update({
         where: { id: id as string },
-        data: { index: req.body.index, title: req.body.title }
+        data: {
+          index: req.body.index,
+          title: req.body.title,
+          cards: {
+            updateMany: cards.map((card: Card) => ({
+              where: { id: card.id },
+              data: { index: card.index }
+            }))
+          }
+        }
       });
-      res.status(200).end();
+      return res.status(200).json(list);
     } catch (error) {
       return res.status(500).json(error);
     }
